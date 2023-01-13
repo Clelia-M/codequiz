e19e1917c298bd2fc5b232074ef16dd135016010
Clelia-M <114869080+Clelia-M@users.noreply.github.com>
2023-01-13 19:04:42 +0000

    startgame function basic code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,9 +3,15 @@ var timer = 60;
 var currentQuestion = 0;
 var score = 0;
 
-// When the start button is clicked, the game starts
+// When the start button is clicked, the game starts (Nb. #start for the start button in the html)
+document.getElementById("start").addEventListener("click", startGame);
 
-// Hide the start screen and show the questions ne by one
+function startGame() {
+    // Hide the start screen and show the question
+
+}
+
+
 
 // Start the timer
 
