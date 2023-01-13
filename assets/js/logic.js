429158c2caa5e30eb2c80d5bf3c73e5beeff553e
Clelia-M <114869080+Clelia-M@users.noreply.github.com>
2023-01-13 18:46:52 +0000

    js timer and score & js questions array

---
+++ assets/js/logic.js
@@ -0,0 +1,5 @@
+//Set the timer intial value, the current question and the score
+var timer = 60;
+var currentQuestion = 0;
+var score = 0;
+
---
+++ assets/js/questions.js
@@ -0,0 +1,57 @@
+// Set arrays for the questions and answers with correct answer for each fo them
+var questions = [
+    { // question 1
+        question: " ",
+        answers: [
+            "",
+            "",
+            "",
+            ""
+        ],
+        correctAnswer: ""
+    },
+
+    { // question 2
+        question: " ",
+        answers: [
+            "",
+            "",
+            "",
+            ""
+        ],
+        correctAnswer: ""
+    },
+
+    { // question 3
+        question:" ",
+        answers: [
+            "",
+            "",
+            "",
+            ""
+        ],
+        correctAnswer: ""
+    },
+
+    { // question 4
+        question:" ",
+        answers: [
+            "",
+            "",
+            "",
+            ""
+        ],
+        correctAnswer: ""
+    },
+
+    { // question 5
+        question:" ",
+        answers: [
+            "",
+            "",
+            "",
+            ""
+        ],
+        correctAnswer: ""
+    },
+];
