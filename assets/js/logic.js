207e5e4eb55975c51e80439ddda9518d69eecefe
Clelia-M <114869080+Clelia-M@users.noreply.github.com>
2023-01-13 19:20:23 +0000

    updateTimer function code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,15 +6,32 @@ var score = 0;
 // When the start button is clicked, the game starts (Nb. button id #start)
 document.getElementById("start").addEventListener("click", startGame);
 
+//Declaring a var for the timer interval
+var intervalId;
+
 function startGame() {
     // Hide the start screen and show the question (Nb. div ids #start-screen & #question)
 document.getElementById("start-screen").classList.add("hide");
 document.getElementById("questions").classList.remove("hide");
 
 // Start the timer
-
-// Check if the timer has reached the zero 
+intervalId = setInterval(updateTimer, 1000);
+updateTimer();
 
 // Display the first question
+displayQuestion();
 }
-// Display the answers
+
+function updateTimer() {
+    timer--;
+    document.getElementById("time").textContent = timer;
+
+    //Check if it has reached 0
+    if (timer === 0) {
+        endGame()
+    }    
+}
+
+
+//Function displayQuestion
+//Function endGame
