4420b718bfd0f215f127461950efb8d42ae73484
Clelia-M <114869080+Clelia-M@users.noreply.github.com>
2023-01-13 19:07:59 +0000

    startGame button action code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,20 +3,18 @@ var timer = 60;
 var currentQuestion = 0;
 var score = 0;
 
-// When the start button is clicked, the game starts (Nb. #start for the start button in the html)
+// When the start button is clicked, the game starts (Nb. button id #start)
 document.getElementById("start").addEventListener("click", startGame);
 
 function startGame() {
-    // Hide the start screen and show the question
-
-}
-
-
+    // Hide the start screen and show the question (Nb. div ids #start-screen & #question)
+document.getElementById("start-screen").classList.add("hide");
+document.getElementById("questions").classList.remove("hide");
 
 // Start the timer
 
 // Check if the timer has reached the zero 
 
 // Display the first question
-
+}
 // Display the answers
