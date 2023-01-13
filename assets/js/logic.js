fb4ca98b5b6c07f37f18fcf8f9079a34fd82a3de
Clelia-M <114869080+Clelia-M@users.noreply.github.com>
2023-01-13 19:01:45 +0000

    logic initial pseudocode

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,3 +3,14 @@ var timer = 60;
 var currentQuestion = 0;
 var score = 0;
 
+// When the start button is clicked, the game starts
+
+// Hide the start screen and show the questions ne by one
+
+// Start the timer
+
+// Check if the timer has reached the zero 
+
+// Display the first question
+
+// Display the answers
