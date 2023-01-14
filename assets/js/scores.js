c09142a392ab6f2213a89fdf41371927cdb20053
Clelia-M <114869080+Clelia-M@users.noreply.github.com>
2023-01-14 09:27:42 +0000

    displayhighscores function fixed

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,73 +1,30 @@
-// document.getElementById("submit").addEventListener("click", function() {
+window.addEventListener("load", displayHighscores);
 
-//     // Get the initials and score from the input fields
-//     var initials = document.getElementById("initials").value;
-//     var score = document.getElementById("final-score").innerHTML;
-
-//     // Create a new highscore object
-//     var highscore = {
-//         initials: initials,
-//         score: score
-//     };
-
-//     // Check if the highscores already exist in local storage
-//     var highscores = JSON.parse(localStorage.getItem("highscores"));
-
-//     // if they don't exist, set highscores to an empty array
-//     if (!highscores) {
-//         highscores = [];
-//     }
-
-//     // Add the new highscore to the array
-//     highscores.push(highscore);
-
-//     // Save the highscores back to local storage
-//     localStorage.setItem("highscores", JSON.stringify(highscores));
-// });
-
-// Display the highscores
-displayHighscores();
-
-// Display the highscores
 function displayHighscores() {
+    //get the highscores from local storage
+    var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+
+    // Sort the highscores by score
+    highscores.sort((a,b) => b.score - a.score);
+
+    // Get the highscores list element
     var highscoresList = document.getElementById("highscores");
 
     // Clear the list
     highscoresList.innerHTML = "";
 
-    // get the exisiting highscores from local storage
-    var highscores = JSON.parse(localStorage.getItem("highscores"));
-
-    // if they don't exist, set highscores to an empty array
-    if (!highscores) {
-        highscores = [];
-    }
-
-    // Sort the highscores by score
-    highscores.sort(function compareScores(a, b) {
-        return b.score - a.score;
-    });
-    
-    // Add the highscores to the list
-    for (var i = 0; i < highscores.lenght; i++) {
-        var highscore = highscores[i];
-        var initials = highscore.initials;
-        var score = highscore.score;
+    // Loop through the highscores
+    highscores.forEach(highscore => {
         var listItem = document.createElement("li");
-        listItem.innerHTML = initials + " - " + score;
+        listItem.innerHTML = `${highscore.initials} - ${highscore.score}`;
         highscoresList.appendChild(listItem);
-    }
+    });
 }
 
+// CLEAR IS WORKING FINE!!!
 // Clear the highscores when the clear button is clicked
 // nb. id for clear button #clear
 document.getElementById("clear").addEventListener("click", clearHighscores);
-
-// // Save a new highscore
-// function saveHighscore(intials, score) {
-//     highscores.push({ initials, score });
-//     localStorage.setItem("highscores", JSON.stringify(highscores));
-// }
 
 // Clear highscore function
 function clearHighscores() {
