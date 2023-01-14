e2c39c6e915cd64a74017442fb6959d69f7bbfef
Clelia-M <114869080+Clelia-M@users.noreply.github.com>
2023-01-14 07:34:49 +0000

    highscore locale storage check code

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,4 +1,16 @@
+var highscores;
+
 // Check if the highscores already exist in local storage
+var highscoresFromLocalStorage = localStorage.getItem("highscores");
+
+//Conditional statement to check if the variable is true anfd if so parse to JSON, otherways empty array
+if (highscoresFromLocalStorage) {
+    highscores = JSON.parse(highscoresFromLocalStorage);
+} else {
+    highscores = [];
+}
+
+
 // Display the highscores
 // Clear the highscores when the clear button is clicked
 // Save a new highscore
