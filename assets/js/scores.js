c63f02a139f5a73df94780b261841e43297c8ccc
Clelia-M <114869080+Clelia-M@users.noreply.github.com>
2023-01-14 07:29:41 +0000

    scores js pseudocode

---
+++ assets/js/scores.js
@@ -0,0 +1,6 @@
+// Check if the highscores already exist in local storage
+// Display the highscores
+// Clear the highscores when the clear button is clicked
+// Save a new highscore
+// Display the highscores
+// Clear highscore function
